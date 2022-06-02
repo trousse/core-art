a05ed12d58bdefe644ac9b1b40ba047df2b4ea79
trousse <ths.rousse@gmail.com>
2022-06-02 17:16:41 +0200

    bug

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'https://github.com/trousse/core-art.git',
       path : '/home/ubuntu/pm2Stuff',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env development',
       'pre-setup': ''
     }
   }
